7fed0ba371eddbbbcf36b5f65128f4dd047633a0
Magomed <mamergov01@bk.ru>
2021-12-18 17:01:50 +0300

    deleted unnecessary stuff

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,44 +1,28 @@
 import React from 'react'
 
-import { createTheme, CssBaseline, Grid, ThemeProvider } from '@mui/material'
-import { red, black } from '@mui/material/colors'
+import { CssBaseline, Grid } from '@mui/material'
 import useStyles from './style'
 import { FormLogin } from '../../components'
-
-const theme = createTheme({
-	// palette: {
-	// 	primary: {
-	// 		light: '#fff',
-	// 		main: 'rgb(23, 105, 170)',
-	// 		dark: '#000',
-	// 	},
-	// 	secondary: {
-	// 		main: '#f44336',
-	// 	},
-	// },
-})
 
 const Login = () => {
 	const classes = useStyles()
 
 	return (
-		<ThemeProvider theme={theme}>
-			<Grid
-				container
-				component="main"
-				sx={{ height: '100vh' }}
-				xs={12}
-				sm={12}
-				lg={12}
-				md={12}
-			>
-				<CssBaseline />
-				<Grid item xs={false} sm={4} md={7} className={classes.image} />
-				<Grid item xs={12} sm={8} md={5} className={classes.form}>
-					<FormLogin />
-				</Grid>
+		<Grid
+			container
+			component="main"
+			sx={{ height: '100vh' }}
+			xs={12}
+			sm={12}
+			lg={12}
+			md={12}
+		>
+			<CssBaseline />
+			<Grid item xs={false} sm={4} md={7} className={classes.image} />
+			<Grid item xs={12} sm={8} md={5} className={classes.form}>
+				<FormLogin />
 			</Grid>
-		</ThemeProvider>
+		</Grid>
 	)
 }
 
